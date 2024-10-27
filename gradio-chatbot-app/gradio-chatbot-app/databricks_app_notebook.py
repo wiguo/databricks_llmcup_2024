ee0e30b7c1fc4e3bbc4049ee41b489b9c723329b
aibabyshark <aibabyshark2024@gmail.com>
2024-10-27 11:24:46 +0800

    extract gradio files

---
+++ gradio-chatbot-app/gradio-chatbot-app/databricks_app_notebook.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### App using serving endpoint 
+# MAGIC <img src ='./databricks_app.png'>
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Simpe Gradio Chatbot
+# MAGIC <img src ='./gradio_ui.png'>
